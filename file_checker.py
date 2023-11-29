21b698b41764996486964a9142d0dfb62c7d4a81
teh-teckstein <treckstein1@gmail.com>
2023-11-29 15:59:32 -0500

    Update file_checker.py

--- file_checker.py
+++ file_checker.py
@@ -1,5 +1,12 @@
 import os
 if __name__ == "__main__":
-    path = "."
-    for root, d_names, f_names in os.walk(path):
-        print(root, d_names, f_names)
+    path = os.path.abspath(os.sep)
+    #path = "."
+    size = 0
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            fp = os.path.join(root, f)
+            size += os.stat(fp).st_size
+ 
+# display size        
+print("Folder size: " + str(size))
