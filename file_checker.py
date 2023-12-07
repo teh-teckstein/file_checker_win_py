bd5f83435848334dc2fcb81f57799b8e10ca6309
teh-teckstein <treckstein1@gmail.com>
2023-12-07 17:06:50 -0500

    Update file_checker.py

--- file_checker.py
+++ file_checker.py
@@ -1,12 +1,118 @@
 import os
-if __name__ == "__main__":
-    path = os.path.abspath(os.sep)
-    #path = "."
-    size = 0
-    for root, dirs, files in os.walk(path):
-        for f in files:
-            fp = os.path.join(root, f)
-            size += os.stat(fp).st_size
+import psutil
+
+def get_size_format(b, factor=1024, suffix="B"):
+#    Scale bytes to its proper byte format
  
-# display size        
-print("Folder size: " + str(size))
+    for unit in ["", "K", "M", "G", "T", "P", "E", "Z"]:
+        if b < factor:
+            return f"{b:.2f}{unit}{suffix}"
+        b /= factor
+    return f"{b:.2f}Y{suffix}"
+
+def get_disk_space_info(drive):
+    #returns used and unused space on the specified drive
+    disk_usage = psutil.disk_usage(drive)._asdict()
+    
+    return {
+        'total_space': get_size_format(disk_usage['total']),
+        'used_space': get_size_format(disk_usage['used']),
+        'unused_space': get_size_format(disk_usage['free']),
+    }
+
+def get_directory_size(directory):
+    #Returns the `directory` size in bytes
+    total = 0
+    try:
+        for entry in os.scandir(directory):
+            if entry.is_file():
+                total += entry.stat().st_size
+            elif entry.is_dir():
+                total += get_directory_size(entry.path)
+    except (NotADirectoryError, PermissionError):
+        return os.path.getsize(directory)
+        print(f"[!] Error accessing or calculating size for directory: {directory}")
+    return total
+
+def get_directory_sizes(base_directory):
+    #Returns a list of directories and their sizes
+    directory_sizes = []
+
+    for entry in os.scandir(base_directory):
+        if entry.is_dir():
+            directory_path = entry.path
+            directory_size = get_directory_size(directory_path)
+            formatted_size = get_size_format(directory_size)
+
+            directory_sizes.append({
+                'path': directory_path,
+                'size': formatted_size,
+                'size_in_bytes': directory_size,  # New: Include the size in bytes for sorting
+            })
+
+    # Sort directories by size in bytes
+    directory_sizes = sorted(directory_sizes, key=lambda x: x['size_in_bytes'], reverse=True)
+
+    return directory_sizes
+
+def display_directory_sizes_and_disk_space(drive):
+    """Displays disk space information and directory sizes."""
+    while True:
+        space_info = get_disk_space_info(drive)
+
+        print(f"\n[+] Total space on C:/: {space_info['total_space']}")
+        print(f"[+] Used space on C:/: {space_info['used_space']}")
+        print(f"[+] Unused space on C:/: {space_info['unused_space']}")
+
+        print("\n[+] Directories:")
+        directory_sizes = get_directory_sizes(drive)
+
+        selected_directory = select_directory(directory_sizes)
+        if selected_directory is None:
+            break
+
+        while True:
+            space_info = get_disk_space_info(drive)
+
+            print(f"\n[+] Total space on C:/: {space_info['total_space']}")
+            print(f"[+] Used space on C:/: {space_info['used_space']}")
+            print(f"[+] Unused space on C:/: {space_info['unused_space']}")
+
+            print("\n[+] Selected Directory:")
+            print(f"{selected_directory}")
+
+            print("\n[+] Subdirectories:")
+            subdirectory_sizes = get_directory_sizes(selected_directory)
+
+            sub_selected_directory = select_directory(subdirectory_sizes)
+            if sub_selected_directory is None:
+                break
+            else:
+                selected_directory = sub_selected_directory
+
+def select_directory(directory_sizes):
+    """Prompt the user to select a directory."""
+    while True:
+        try:
+            print("\n[+] Directories:")
+            for i, entry in enumerate(directory_sizes, start=1):
+                print(f"{i}. {entry['path']}: Size - {entry['size']}")
+
+            user_input = input("Enter the number of the directory to scan (or 'exit' to quit): ")
+            if user_input.lower() == 'exit':
+                break
+                return None
+            elif user_input.isdigit():
+                index = int(user_input)
+                if 1 <= index <= len(directory_sizes):
+                    return directory_sizes[index - 1]['path']
+                else:
+                    print("Invalid directory number. Please enter a valid number.")
+            else:
+                print("Invalid input. Please enter a valid number.")
+        except Exception as e:
+            print(f"Error: {e}")
+
+if __name__ == "__main__":
+    drive = input("Enter the drive to scan (default, C:/):").upper() or "C:/"
+    display_directory_sizes_and_disk_space(drive)
